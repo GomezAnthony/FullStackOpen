97f255190bae64ede92390215b3898a0ac8d2b13
Anthony Gomez <gomez9162@gmail.com>
2024-07-15 00:15:16 -0500

    Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 
 const Display = () => {
@@ -8,14 +9,41 @@ const Display = () => {
   );
 };
 
+const Button = ({ onClick, label }) => {
+  return (
+    <div>
+      <button onClick={onClick}>{label}</button>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <div>
       <Display />
+      <Button label="Good" onClick={handleGoodClick} />
+      <Button label="Neutral" onClick={handleNeutralClick} />
+      <Button label="Bad" onClick={handleBadClick} />
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   );
 };
