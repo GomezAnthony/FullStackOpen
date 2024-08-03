8d39152e27b4fffdec4cac796306549e9967d4a0
Anthony Gomez <gomez9162@gmail.com>
2024-08-02 23:58:51 -0500

    Updated Backend

--- part1/phonebook-backend/index.js
+++ part1/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 let notes = [
   { id: '1', content: 'HTML is easy', important: true },
   { id: '2', content: 'Browser can execute only JavaScript', important: false },
@@ -28,6 +31,19 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post('/api/notes/', (request, response) => {
+  const note = request.body;
+  console.log(note);
+  response.json(note);
 });
 
 const PORT = 3001;
