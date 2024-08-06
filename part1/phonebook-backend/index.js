ff07d6385ed637f31a3197ff507010ad8b5e7bf7
Anthony Gomez <gomez9162@gmail.com>
2024-08-06 11:26:24 -0500

    Need to fix backend

--- part1/phonebook-backend/index.js
+++ part1/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -103,7 +106,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.envPORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
