138d0ca881ad4f7c036536d8d1404dee4ddab2ee
Anthony Gomez <56603329+GomezAnthony@users.noreply.github.com>
2024-09-15 00:41:15 -0500

    Added JWT

--- part4/blogs-backend/app.js
+++ part4/blogs-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const blogsRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
